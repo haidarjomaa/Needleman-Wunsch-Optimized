4cc87f8c2b906cdcac88501db78b51c70e5d87c7
Haidar Jomaa <130698588+haidarjomaa@users.noreply.github.com>
2024-12-01 00:01:22 +0200

    Create read_seq.py

---
+++ sequence_align/read_seq.py
@@ -0,0 +1,16 @@
+def read_genome(file1, file2):
+    with open(file1,"r") as f:
+        strings = f.readlines()
+    strings = strings[1:]
+    strings = [i.strip() for i in strings]
+    result1 = ""
+    for i in strings:
+        result1 += i
+    with open(file2,"r") as f:
+        strings = f.readlines()
+    strings = strings[1:-4]
+    strings = [i.strip() for i in strings]
+    result2 = ""
+    for i in strings:
+        result2 += i
+    return result1, result2
