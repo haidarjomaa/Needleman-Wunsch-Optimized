2238cd3a8fe3b0a49fbfbd03fed763d62f929322
Haidar Jomaa <130698588+haidarjomaa@users.noreply.github.com>
2024-12-01 23:59:00 +0200

    Update efficient_pairwise_align.py

--- sequence_align/efficient_pairwise_align.py
+++ sequence_align/efficient_pairwise_align.py
@@ -1,19 +1,24 @@
 import multiprocessing
 
+#for each row we have this function that the process will run
 def process_row(row, prev_row, match_reward: int, mismatch_penalty: int,
                 indel_penalty: int, s: str, t: str, index):
+    #for the first row on the exterior - we only have gaps in it
     if index == 0:
       for i in range(1, len(s)+1):
         row[i] = row[i-1] + indel_penalty
-
+    #for the rest of the rows
     else:
       for i in range(1, len(row)):
+        #if we have a match between the 2 strings (while respecting the order of fixed t - moving s)
         if s[index+i-1] == t[index-1]:
           row[i] = max(prev_row[i]+match_reward, prev_row[i+1]+indel_penalty, row[i-1]+indel_penalty)
+        #otherwise just use the mismatch penalty
         else:
           row[i] = max(prev_row[i]+mismatch_penalty, prev_row[i+1]+indel_penalty, row[i-1]+indel_penalty)
     return row
 
+#this one has the exact same description to the row function with some name and variable changes
 def process_col(col, prev_col, match_reward: int, mismatch_penalty: int,
                 indel_penalty: int, s: str, t: str, index):
     if index == 0:
@@ -29,30 +34,20 @@ def process_col(col, prev_col, match_reward: int, mismatch_penalty: int,
 
 def global_alignment(match_reward: int, mismatch_penalty: int, indel_penalty: int,
                      s: str, t: str):
-    """
-    Compute the global alignment of two strings based on given rewards and penalties.
-
-    Args:
-    match_reward (int): The reward for a match between two characters.
-    mismatch_penalty (int): The penalty for a mismatch between two characters.
-    indel_penalty (int): The penalty for an insertion or deletion.
-    s (str): The first string.
-    t (str): The second string.
-
-    Returns:
-    Tuple[int, str, str]: A tuple containing the alignment score and the aligned strings.
-    """
     m = len(s)
     n = len(t)
     prev_row = [0]*(m+1)
     prev_col = [0]*(n+1)
-
+    #create a pool of 2 processes for the multiprocessing
     with multiprocessing.Pool(processes=2) as pool:
+        #loop over the elements based on the the string values
         for i in range(max(1+m, 1+n)):
+            #initialize the current rows
             row = [0]*(m+1-i)
             col = [0]*(n+1-i)
             if i != 0:
               if i < m+1 and i < n+1:
+                #initializing the common value between row/col
                 if s[i-1] == t[i-1]:
                  row[0] = max(prev_row[0]+match_reward, prev_row[1]+indel_penalty, prev_col[1]+indel_penalty)
                  col[0] = max(prev_row[0]+match_reward, prev_row[1]+indel_penalty, prev_col[1]+indel_penalty)
@@ -75,10 +70,10 @@ def global_alignment(match_reward: int, mismatch_penalty: int, indel_penalty: in
               prev_col = pool.apply_async(process_col, args=(col, prev_col, match_reward, mismatch_penalty, indel_penalty,
                     s, t, i,))
               prev_col = prev_col.get()
-
+        #wait until the 2 finish their tasks
         pool.close()
         pool.join()
-
+    #return the final node of the DP graph
     if len(prev_row) > len(prev_col):
       return prev_row[-1]
     else:
